5dbd6869fac4cc831c8a5d51b53ae99dc3ae2190
snickbit <nicklowe@snickbit.com>
2022-05-18 21:45:59 -0400

    chore: bump feathers versions

--- packages/feathers-redis/src/index.ts
+++ packages/feathers-redis/src/index.ts
@@ -1,29 +1,27 @@
-import {AdapterService, ServiceOptions, sorter} from '@feathersjs/adapter-commons'
+import {AdapterBase, AdapterServiceOptions, filterQuery, sorter} from '@feathersjs/adapter-commons'
 import {Conflict, Unprocessable} from '@feathersjs/errors'
 import {filterResults} from '@snickbit/feathers-helpers'
 import {isObject, isPrimitive, merge, parse, uuid} from '@snickbit/utilities'
 import sift from 'sift'
 import Redis, {RedisKey, RedisOptions} from 'ioredis'
-import {Application, Paginated, Params} from '@feathersjs/feathers'
+import {Application, Paginated, Params, Query} from '@feathersjs/feathers'
+import {AdapterParams, PaginationOptions} from '@feathersjs/adapter-commons/src/declarations'
 
-export interface RedisServiceOptions extends ServiceOptions {
+export interface RedisServiceOptions<T = any> extends AdapterServiceOptions {
 	connection: RedisOptions
-	matcher?: (query: any) => any;
-	sorter?: (sort: any) => any;
+	matcher?: (query: T) => T;
+	sorter?: (sort: T) => T;
 }
 
 export type RedisId = string
-export type NullableRedisId = string | null
+export type NullableRedisId = RedisId | null
 
-export class RedisService extends AdapterService {
-	public query: any
-	declare options: RedisServiceOptions
-	public filterQuery: any
+export class RedisService<T = any, D = Partial<T>, O extends RedisServiceOptions = RedisServiceOptions> extends AdapterBase {
+	declare options: O
 	client: Redis
 
-	constructor(options: RedisOptions, app: Application) {
-		super({})
-		this.options = Object.assign({
+	constructor(options: O, app: Application) {
+		options = Object.assign({
 			id: 'id',
 			events: [],
 			paginate: {},
@@ -32,28 +30,30 @@ export class RedisService extends AdapterService {
 			whitelist: [],
 			matcher: sift,
 			sorter
-		}, options) as RedisServiceOptions
+		}, options)
+
+		super(options)
 
 		const connection = this.options.connection || (app && app.get('redis')) || {}
 		if (connection.keyPrefix && !connection.keyPrefix.endsWith('_')) connection.keyPrefix += '_'
 		this.client = new Redis(connection)
 	}
 
-	get keyPrefix() {
+	get keyPrefix(): string {
 		return this.options?.connection?.keyPrefix || ''
 	}
 
-	protected parseId(id: RedisId) {
-		if (id && this.keyPrefix) return id.replace(new RegExp('^' + this.keyPrefix), '')
+	protected parseId(id: RedisId): string {
+		if (id && this.keyPrefix) return String(id).replace(new RegExp('^' + this.keyPrefix), '')
 		return id
 	}
 
-	protected isMulti(id: RedisId, params?: Params) {
-		const {query} = this.filterQuery(params)
+	protected isMulti(id: RedisId, params?: Params): Query | false {
+		const {query} = filterQuery(params)
 		return !id && query && Object.keys(query).length ? query : false
 	}
 
-	protected prepareResults(id: RedisId, value: any) {
+	protected prepareResults(id: RedisId, value: string | T) {
 		const data = {
 			key: id,
 			value: parse(value)
@@ -61,7 +61,9 @@ export class RedisService extends AdapterService {
 		return parse(data)
 	}
 
-	async _find(params: Params): Promise<any | any[] | Paginated<any>> {
+	async $find(params?: AdapterParams & { paginate?: PaginationOptions }): Promise<Paginated<T>>;
+	async $find(params?: AdapterParams & { paginate: false }): Promise<T[]>
+	async $find(params: AdapterParams = {} as AdapterParams): Promise<Paginated<T> | T[]> {
 		return new Promise(resolve => {
 			const stream = this.client.scanStream()
 			const results = []
@@ -70,7 +72,7 @@ export class RedisService extends AdapterService {
 				if (this.keyPrefix) resultKeys = resultKeys.filter(key => key.startsWith(this.keyPrefix))
 				for (let key of resultKeys) {
 					if (!results.includes(key)) {
-						get_promises.push(this._get(key))
+						get_promises.push(this.$get(key))
 						results.push(key)
 					}
 				}
@@ -79,54 +81,79 @@ export class RedisService extends AdapterService {
 		})
 	}
 
-	async _multi(method: string, params?: Params) {
-		const items = await this._find(params)
-		return Promise.all(items.map(item => this[method](item[this.id])))
-	}
-
-	async _get(id: RedisId, params?: Params): Promise<any> {
-		const {query} = this.filterQuery(params)
+	async $get(id: RedisId, params?: AdapterParams): Promise<T> {
+		const {query} = filterQuery(params)
 		id = this.parseId(id)
 		if (query && Object.keys(query).length) {
 			if (id) params.query[this.id] = id
 			params.query.$limit = 1
-			return this._find({query: {[this.id]: id}, paginate: false}).then(data => this.prepareResults(id, data.pop()))
+			return this.$find({
+				query: {
+					[this.id]: id
+				},
+				paginate: false
+			}).then(data => this.prepareResults(id, data.pop()))
 		} else {
 			const value = await this.client.get(id as RedisKey)
 			return this.prepareResults(id, value)
 		}
 	}
 
+	async $create(data: D, params?: AdapterParams): Promise<T>;
+	async $create(data: D[], params?: AdapterParams): Promise<T[]>;
+	async $create(data: D | D[], _params?: AdapterParams): Promise<T | T[]>;
+	async $create(data: D | D[], params: AdapterParams = {} as AdapterParams): Promise<T | T[]> {
+		let key = (isObject(data) && data[this.id] ? data[this.id] : undefined)
+		if (key && await this.$get(key, params)) throw new Conflict(`Redis value with key ${key} already exists`)
+		if (!key) key = uuid()
+		await this.$set(key, data, params)
+		return this.$get(key, params)
+	}
+
+	async $update(id: RedisId, data: D, params: AdapterParams = {} as AdapterParams): Promise<T> {
+		id = this.parseId(id)
+		await this.$set(id, data, params)
+		return this.$get(id, params)
+	}
+
+	async $patch(id: null, data: D, params?: AdapterParams): Promise<T[]>;
+	async $patch(id: RedisId, data: D, params?: AdapterParams): Promise<T>;
+	async $patch(id: NullableRedisId, data: D, _params?: AdapterParams): Promise<T | T[]>;
+	async $patch(id: NullableRedisId, data: D, params: AdapterParams = {} as AdapterParams): Promise<T | T[]> {
+		if (this.isMulti(id, params)) return this.$multi('$patch', params)
+		id = this.parseId(id)
+		const old_data = this.$get(id, params)
+		const new_data = merge(old_data, data) as D
+		await this.$set(id, new_data, params)
+		return this.$get(id, params)
+	}
+
+	async $remove(id: null, params?: AdapterParams): Promise<T[]>;
+	async $remove(id: RedisId, params?: AdapterParams): Promise<T>;
+	async $remove(id: NullableRedisId, _params?: AdapterParams): Promise<T | T[]>;
+	async $remove(id: NullableRedisId, params: AdapterParams = {} as AdapterParams): Promise<T | T[]> {
+		id = this.parseId(id)
+
+		if (this.isMulti(id, params)) return this.$multi('$remove', params)
+
+		const entry = await this.$get(id, params)
+
+		await this.client.del(id)
+
+		return entry
+	}
+
+
+	async $multi(method: string, params?: Params): Promise<T[]> {
+		const items = await this.$find({...params, paginate: false}) as unknown as T[]
+		return Promise.all(items.map(item => this[method](item[this.id])))
+	}
+
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	async _set(key, data: any, _params?: Params): Promise<any> {
+	async $set(key, data: D | D[], _params?: Params): Promise<string> {
 		if (!key) throw new Unprocessable('id is required')
 		const value = stringifyValue(data)
 		return this.client.set(key, value)
-	}
-
-	async _create(data, params?: Params): Promise<any | any[]> {
-		const key = (isObject(data) && data[this.id] ? data[this.id] : undefined)
-		if (key && await this._get(key, params)) throw new Conflict(`Redis value with key ${key} already exists`)
-		return this._set(uuid(), data, params)
-	}
-
-	async _update(id: RedisId, data: any, params?: Params): Promise<any> {
-		id = this.parseId(id)
-		return this._set(id, data, params)
-	}
-
-	async _patch(id: NullableRedisId, data: any, params?: Params): Promise<any | any[]> {
-		if (this.isMulti(id, params)) return this._multi('_patch', params)
-		id = this.parseId(id)
-		const old_data = this._get(id, params)
-		const new_data = merge(old_data, data)
-		return this._set(id, new_data, params)
-	}
-
-	async _remove(id: NullableRedisId, params?: Params): Promise<any | any[]> {
-		if (this.isMulti(id, params)) return this._multi('_remove', params)
-		id = this.parseId(id)
-		return this.client.del(id)
 	}
 }
 
