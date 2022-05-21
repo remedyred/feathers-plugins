2f8350abeec4c87e968bbf4c109878a3858faeb9
snickbit <nicklowe@snickbit.com>
2022-05-20 23:30:23 -0400

    init: mongodb plugin for feathers-up

---
+++ packages/feathers-up-mongodb/README.md
@@ -0,0 +1,29 @@
+# @snickbit/feathers-up-mongodb
+
+<!--START_SECTION:readmes-description-->
+
+MongoDB plugin for @snickbit/feathers-up
+
+<!--END_SECTION:readmes-description-->
+
+## Installation
+
+```bash
+yarn add @snickbit/feathers-up-mongodb
+```
+
+```bash
+npm add @snickbit/feathers-up-mongodb
+```
+
+## Documentation
+
+### [API Docs](https://github.com/snickbit/feathers/blob/main/packages/feathers-up-mongodb/README.md)
+
+### [CHANGELOG](https://github.com/snickbit/feathers/blob/main/packages/feathers-up-mongodb/CHANGELOG.md)
+
+## License
+
+Copyright (c) 2022 - **Nicholas Lowe** aka **Snickbit**
+
+[MIT License](https://github.com/snickbit/feathers/blob/main/LICENSE)
---
+++ packages/feathers-up-mongodb/src/index.ts
@@ -0,0 +1,43 @@
+import {MongoClient} from 'mongodb'
+import {Application} from '@snickbit/feathers-up'
+
+export interface MongoClientOptions {
+	uri?: string
+	database?: string
+	authSource?: string,
+	auth?: {
+		username?: string
+		password?: string
+	}
+}
+
+export default async function (app: Application, config: MongoClientOptions | MongoClientOptions[]) {
+	if (!app.get('mongoClient')) {
+		app.out.verbose('Connect to MongoDB database...')
+		if (Array.isArray(config)) {
+			const promises = []
+			for (let mongodb of config) {
+				const mongoClientPromise = connectToMongoDB(app, mongodb)
+				app.set(`mongoClientPromise.${mongodb.database}`, mongoClientPromise)
+				app.set(`mongoClient.${mongodb.database}`, mongoClientPromise.then(client => client.db(mongodb.database)))
+				promises.push(mongoClientPromise)
+			}
+			await Promise.all(promises)
+		} else {
+			const mongoClientPromise = connectToMongoDB(app, config)
+			app.set('mongoClientPromise', mongoClientPromise)
+			app.set('mongoClient', mongoClientPromise.then(client => client.db(config.database)))
+		}
+	}
+	return app.get('mongoClient')
+}
+
+export function connectToMongoDB(app: Application, config: MongoClientOptions): Promise<MongoClient> {
+	const mongoOptions = {
+		authSource: config.authSource || config.database,
+		auth: config.auth
+	}
+	const mongoClientPromise = MongoClient.connect(config.uri, mongoOptions)
+	mongoClientPromise.catch(err => app.out.error(err))
+	return mongoClientPromise
+}
