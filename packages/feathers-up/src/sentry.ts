ae5a7d7b3db091d22918ee789fafe3204eaca9e5
Snickbit <snickbit@users.noreply.github.com>
2022-06-08 11:36:23 -0400

    fix: improve sentry configuration

--- packages/feathers-up/src/sentry.ts
+++ packages/feathers-up/src/sentry.ts
@@ -5,10 +5,18 @@ import * as Sentry from '@sentry/node'
 export function sentryInit(app: Application) {
 	const sentry = useSetup('sentry')
 	if (sentry) {
-		Sentry.init({
-			dsn: sentry.dsn,
-			environment: process.env.NODE_ENV
-		})
+		const sentryConfig = {
+			autoSessionTracking: false,
+			debug: process.env.NODE_ENV === 'development',
+			environment: process.env.NODE_ENV,
+			...sentry
+		}
+
+		if (app.get('version')) {
+			sentryConfig.release = app.get('version')
+		}
+
+		Sentry.init(sentryConfig)
 
 		if (app.get('appType') === 'server') {
 			app.out.verbose('Enable sentry request handler')
